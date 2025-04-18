51b89f7268671789a517f2479a0a74e50cec5f7d
davidbirch4272 <davidbirch4272@gmail.com>
2025-04-18 10:25:33 -0400

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,37 @@
-import React, { useEffect } from "react";  
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import User from "../Components/user.jsx";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const [users, SetUsers] = useState([]);
 
-    
-    async function fetchUsers() {
-        const { data } = await axios.get("https://jsonplaceholder.typicode.com/")
-        console.log(data)
-    }
-    useEffect(() => {
+  async function fetchUsers() {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    SetUsers(data);
+    console.log(data);
+  }
+  useEffect(() => {
+    setTimeout(() => {}, 500);
     fetchUsers();
-    }, []);
+  }, []);
 
-    return <h1>Home</h1>
+    return (
+    <div>
+      {users.map((user) => (
+      <Link to={`/users/${user.id}`} key={user.id} >
+      <User 
+      id={user.id} 
+      name={user.name} 
+      email={user.email} 
+      username={user.username} 
+      />
+      </Link>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
